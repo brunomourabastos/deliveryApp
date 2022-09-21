2be2a378856290ced4d11e2a1c51e7004c098b02
Carlos <nashlasterby@gmail.com>
2022-09-21 02:23:16 -0300

    userRoutes concluded

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,28 @@
+require('express-async-errors');
 const express = require('express');
+const cors = require('cors');
+const { globalError } = require('../middlewares/globalError');
+const { routes } = require('./routes');
 
 const app = express();
 
-app.get('/coffee', (_req, res) => res.status(418).end());
+const PORT = 3001;
+
+class App {
+  constructor(app = express()) {
+    this.app = app;
+    this.app.use(express.json());
+    this.app.use(cors());
+    this.app.use(routes);
+    this.app.use(globalError.handle);
+    this.app.use(express.static('public'));
+  }
+
+  startServer() {
+    this.app.listen(PORT, () => console.log(`API rodando na porta ${PORT}`));
+  }
+}
+
+// app.get('/coffee', (_req, res) => res.status(418).end());
 
 module.exports = app;
