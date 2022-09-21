a41fc79df10648446b1544241164191e7dc2cdd7
Carlos <nashlasterby@gmail.com>
2022-09-20 22:57:45 -0300

    models refactored, utils paste

--- back-end/src/database/models/Products.js
+++ back-end/src/database/models/Products.js
@@ -1,23 +1,54 @@
 const { Model, DataTypes } = require('sequelize');
-const db = require('./index');
+// const db = require('./index');
 
-class Products extends Model {
-  static init(sequelize) {
-    super.init({
-      name: DataTypes.STRING,
-      price: DataTypes.DECIMAL(4, 2),
-      urlImage: DataTypes.STRING
-    }, {
-      underscored: true,
-      timestamps: false,
-      modelName: 'Products',
-      sequelize
-    });
-  }
+// class Products extends Model {
+//   static init(sequelize) {
+//     super.init({
+//       name: DataTypes.STRING,
+//       price: DataTypes.DECIMAL(4, 2),
+//       urlImage: DataTypes.STRING
+//     }, {
+//       underscored: true,
+//       timestamps: false,
+//       modelName: 'Products',
+//       sequelize
+//     });
+//   }
 
-  static associate(db) {
-    this.belongsToMany(db.Sales, { foreignKey: 'product_id', through: 'sales_products', as: 'sales' });
+//   static associate(db) {
+//     this.belongsToMany(db.Sales, { foreignKey: 'product_id', through: 'sales_products', as: 'sales' });
+//   }
+// }
+
+class Products extends Model {}
+
+Products.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(4, 2),
+      allowNull: false,
+    },
+    urlImage: {
+      type: DataTypes.STRING(200),
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: false,
+    underscored: true,
+    modelName: "Products",
+    sequelize,
   }
-}
+);
 
 module.exports = Products;
